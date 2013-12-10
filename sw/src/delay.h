1b0d7e95084a8ef99471a3321b99d83ec2489ac5
noah <noah@noah.noah>
2013-12-10 18:16:19 -0500

    - break out oled task. working ok, but some spots are flickering on refresh.
    Former-commit-id: fb179e5078c523fdc1b06ccaee3138d64634f677 [formerly fb179e5078c523fdc1b06ccaee3138d64634f677 [formerly 4fd807b14c605efe994bcd909e894b0ec61a71d0]]
    Former-commit-id: dccb1fdd337d551eacf0ca386da85b84635a7c5b
    Former-commit-id: 6ddd078b77f91c5167911983417a9c57d84da22b

---
+++ sw/src/delay.h
@@ -0,0 +1,14 @@
+
+//
+// delay.h
+// Header file for delay.
+//
+
+#if !defined(DELAY_H)
+#define DELAY_H
+
+#include <stdint.h>
+
+void delay_ms(uint32_t ms);
+
+#endif // SSD1306_H
---
+++ sw/src/oled.h
@@ -0,0 +1,15 @@
+
+//
+// oled.h
+// Header file for oled.
+//
+
+#include <stdint.h>
+
+#if !defined(OLED_H)
+#define OLED_H
+
+// call to refresh the display
+void oled_refresh(void);
+
+#endif // OLED_H
