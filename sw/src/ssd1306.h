a03d6d2d448f4d140fc285b131a42c4c4052b5fd
noahp <noah@noah.noah>
2013-12-16 19:59:55 -0500

    -add sw directory from branch oled_brkout
    Former-commit-id: e26bfdd3349df70303f87d86dbec4721a1a1e5d9 [formerly e26bfdd3349df70303f87d86dbec4721a1a1e5d9 [formerly 2187a58c0d69b7bc4c6077dc3c91d1d5fb943d0c]]
    Former-commit-id: 25ceb08097f7a1c8dba86572119d29e6f744cf91
    Former-commit-id: 62fe435b4337ca90f8730f665d3f254c23dd77ad

---
+++ sw/src/ssd1306.h
@@ -0,0 +1,16 @@
+
+//
+// ssd1306.h
+// Header file for ssd1306 oled controller.
+//
+
+#if !defined(SSD1306_H)
+#define SSD1306_H
+
+#include <stdint.h>
+
+void ssd1306_init(void);
+void ssd1306_writeBuffer(void);
+void ssd1306_writeString(char *str, uint32_t pos);
+
+#endif // SSD1306_H
